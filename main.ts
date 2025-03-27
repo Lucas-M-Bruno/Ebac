307dc7c05fcdf2b22f0163228e02f9337e94186e
Lucas Moura Bruno <165830297+Lucas-M-Bruno@users.noreply.github.com>
2025-03-27 15:39:51 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+function multiplicar(x:number, y:number):number {
+    return x * y;
+}
+
+console.log(multiplicar(5,5));
+
+
+function concatenar(nome: string): string {
+    return "ola "+ nome;
+}
+
+console.log(concatenar("lucas"));
